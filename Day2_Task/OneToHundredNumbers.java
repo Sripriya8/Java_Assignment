586fd8619c6c8b64491c1e55957985aa41e57231
Sripriya8 <sripriyachinthakindi150@gmail.com>
2025-08-06 15:18:58 +0530

    Day2 - Java Assignment on Arrays and Strings

---
+++ Day2_Task/OneToHundredNumbers.java
@@ -0,0 +1,11 @@
+public class OneToHundredNumbers{
+    public static void main(String args[]){
+        
+        System.out.println("Numbers from 1-100");
+        for(int i='B'-'A'; i<='d'; i++){
+            System.out.
+            print(i+ " ");
+        }
+        
+    }
+}
